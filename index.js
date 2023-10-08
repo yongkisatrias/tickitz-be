7c320304b257eda6514a9c2af4f4cc4f558234dd
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-10-08 18:20:17 +0700

    [UPDATE} Update endpoint cinema (new cinema)

--- index.js
+++ index.js
@@ -165,7 +165,7 @@ app.get("/cinemas", async (req, res) => {
   }
 });
 
-// Get Selected Cinema (/cinema/:id)
+// Get Selected Cinema (/cinemas/:id)
 app.get("/cinemas/:id", async (req, res) => {
   try {
     const { id } = req.params;
@@ -176,6 +176,41 @@ app.get("/cinemas/:id", async (req, res) => {
       message: "Get data success",
       data: request,
     });
+  } catch (error) {
+    res.status(502).json({
+      status: false,
+      message: "Something wrong in our server",
+      data: [],
+    });
+  }
+});
+
+// New Cinema (/cinemas)
+app.post("/cinemas", async (req, res) => {
+  try {
+    const { movie_id, name, city, addres, show_times, price, logo } = req.body;
+
+    const isInputValid = movie_id && name && city && addres && show_times && price && logo;
+
+    // check if input is valid
+    if (!isInputValid) {
+      res.status(400).json({
+        status: false,
+        message: "Bad input, please make sure your input is completed",
+      });
+    }
+
+    const request = await database`INSERT INTO cinemas
+      (movie_id, name, city, addres, show_times, price, logo)
+    values
+      (${movie_id}, ${name}, ${city}, ${addres}, ${show_times}, ${price}, ${logo}) RETURNING id`;
+
+    if (request.length > 0) {
+      res.status(201).json({
+        status: true,
+        message: "Insert data success",
+      });
+    }
   } catch (error) {
     res.status(502).json({
       status: false,
@@ -390,151 +425,3 @@ app.put("/users/edit/password", checkJwt, async (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`);
 });
-
-// // MOVIES START
-// app.get("/movies", async (req, res) => {
-//   try {
-//     const request = await database`SELECT id, name, duration, genres, poster FROM movies`;
-
-//     res.status(200).json({
-//       status: true,
-//       message: "Get data success",
-//       data: request,
-//     });
-//   } catch (error) {
-//     res.status(502).json({
-//       status: false,
-//       message: "Something wrong in our server",
-//       data: [],
-//     });
-//   }
-// });
-
-// app.get("/movies/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const request = await database`SELECT * FROM movies WHERE id = ${id}`;
-
-//     res.status(200).json({
-//       status: true,
-//       message: "Get data success",
-//       data: request,
-//     });
-//   } catch (error) {
-//     res.status(502).json({
-//       status: false,
-//       message: "Something wrong in our server",
-//       data: [],
-//     });
-//   }
-// });
-
-// app.post("/movies", async (req, res) => {
-//   try {
-//     const { name, release_date, duration, genres, directed_by, casts, synopsis, poster } = req.body;
-
-//     const isInputValid = name && release_date && duration && genres && directed_by && casts && synopsis && poster;
-
-//     // check if input is valid
-//     if (!isInputValid) {
-//       res.status(400).json({
-//         status: false,
-//         message: "Bad input, please make sure your input is completed",
-//       });
-//     }
-
-//     const request = await database`INSERT INTO movies
-//       (name, release_date, duration, genres, directed_by, casts, synopsis, poster)
-//     values
-//       (${name}, ${release_date}, ${duration}, ${genres}, ${directed_by}, ${casts}, ${synopsis}, ${poster}) RETURNING id`;
-
-//     if (request.length > 0) {
-//       res.status(201).json({
-//         status: true,
-//         message: "Insert data success",
-//       });
-//     }
-//   } catch (error) {
-//     res.status(502).json({
-//       status: false,
-//       message: "Something wrong in our server",
-//       data: [],
-//     });
-//   }
-// });
-// // MOVIES END
-
-// // --------------------------------- //
-
-// // CINEMA START
-// app.get("/cinemas", async (req, res) => {
-//   try {
-//     const request = await database`SELECT id, movie_id, name, city, addres, price, logo FROM cinemas`;
-
-//     res.status(200).json({
-//       status: true,
-//       message: "Get data success",
-//       data: request,
-//     });
-//   } catch (error) {
-//     res.status(502).json({
-//       status: false,
-//       message: "Something wrong in our server",
-//       data: [],
-//     });
-//   }
-// });
-
-// app.get("/cinemas/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const request = await database`SELECT * FROM cinemas WHERE id = ${id}`;
-
-//     res.status(200).json({
-//       status: true,
-//       message: "Get data success",
-//       data: request,
-//     });
-//   } catch (error) {
-//     res.status(502).json({
-//       status: false,
-//       message: "Something wrong in our server",
-//       data: [],
-//     });
-//   }
-// });
-
-// app.post("/cinemas", async (req, res) => {
-//   try {
-//     const { movie_id, name, city, addres, show_times, price, logo } = req.body;
-
-//     const isInputValid = movie_id && name && city && addres && show_times && price && logo;
-
-//     // check if input is valid
-//     if (!isInputValid) {
-//       res.status(400).json({
-//         status: false,
-//         message: "Bad input, please make sure your input is completed",
-//       });
-//     }
-
-//     const request = await database`INSERT INTO cinemas
-//       (movie_id, name, city, addres, show_times, price, logo)
-//     values
-//       (${movie_id}, ${name}, ${city}, ${addres}, ${show_times}, ${price}, ${logo}) RETURNING id`;
-
-//     if (request.length > 0) {
-//       res.status(201).json({
-//         status: true,
-//         message: "Insert data success",
-//       });
-//     }
-//   } catch (error) {
-//     res.status(502).json({
-//       status: false,
-//       message: "Something wrong in our server",
-//       data: [],
-//     });
-//   }
-// });
-// // CINEMA END
