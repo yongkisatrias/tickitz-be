fc4859ce327cfde0e0199b9d527724366c48122d
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-10-09 19:04:55 +0700

    [UPDATE] create validation for cinemas in routers folder

--- routers/cinemas.js
+++ routers/cinemas.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const cinemasModel = require("../models/cinemas");
+const { Validator } = require("node-input-validator");
 
 // -- Endpoint Cinemas -- //
 // Get All Cinemas (/cinemas)
@@ -42,73 +43,115 @@ router.get("/cinemas/:id", async (req, res) => {
 });
 
 // New Cinema (/cinemas)
-router.post("/cinemas", async (req, res) => {
-  try {
-    const { movie_id, name, city, addres, show_times, price, logo } = req.body;
+router.post(
+  "/cinemas",
+  async (req, res, next) => {
+    // schema
+    const schema = new Validator(req.body, {
+      movie_id: "required|integer",
+      name: "required|minLength:1|maxLength:100",
+      city: "required|minLength:1|maxLength:100",
+      addres: "required|minLength:1|maxLength:100",
+      show_times: "required|array|arrayUnique",
+      price: "required|integer",
+      logo: "required|url",
+    });
 
-    const isInputValid = movie_id && name && city && addres && show_times && price && logo;
+    schema.check().then((matched) => {
+      if (!matched) {
+        res.status(422).json({
+          status: false,
+          message: schema.errors,
+          data: null,
+        });
+      } else {
+        next();
+      }
+    });
+  },
+  async (req, res) => {
+    try {
+      const { movie_id, name, city, addres, show_times, price, logo } = req.body;
 
-    // check if input is valid
-    if (!isInputValid) {
-      res.status(400).json({
+      const request = await cinemasModel.newCinema({
+        movie_id,
+        name,
+        city,
+        addres,
+        show_times,
+        price,
+        logo,
+      });
+
+      if (request.length > 0) {
+        res.status(201).json({
+          status: true,
+          message: "Insert cinema success",
+        });
+
+        return;
+      }
+    } catch (error) {
+      res.status(502).json({
         status: false,
-        message: "Bad input, please make sure your input is completed",
+        message: "Something wrong in our server",
+        data: [],
       });
-
-      return;
     }
-
-    const request = await cinemasModel.newCinema({
-      movie_id,
-      name,
-      city,
-      addres,
-      show_times,
-      price,
-      logo,
-    });
-
-    if (request.length > 0) {
-      res.status(201).json({
-        status: true,
-        message: "Insert cinema success",
-      });
-
-      return;
-    }
-  } catch (error) {
-    res.status(502).json({
-      status: false,
-      message: "Something wrong in our server",
-      data: [],
-    });
   }
-});
+);
 
 // Update Cinema (cinemas/:id)
-router.put("/cinemas/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const columns = ["movie_id", "name", "city", "addres", "show_times", "price", "logo"];
-
-    const request = await cinemasModel.updateCinema(req.body, columns, id);
-
-    if (request.length > 0) {
-      res.status(202).json({
-        status: true,
-        message: "Update cinema success",
-      });
-
-      return;
-    }
-  } catch (error) {
-    res.status(502).json({
-      status: false,
-      message: "Something wrong in our server",
-      data: [],
+router.put(
+  "/cinemas/:id",
+  async (req, res, next) => {
+    // schema
+    const schema = new Validator(req.body, {
+      movie_id: "required|integer",
+      name: "required|minLength:1|maxLength:100",
+      city: "required|minLength:1|maxLength:100",
+      addres: "required|minLength:1|maxLength:100",
+      show_times: "required|array|arrayUnique",
+      price: "required|integer",
+      logo: "required|url",
     });
+
+    schema.check().then((matched) => {
+      if (!matched) {
+        res.status(422).json({
+          status: false,
+          message: schema.errors,
+          data: null,
+        });
+      } else {
+        next();
+      }
+    });
+  },
+  async (req, res) => {
+    try {
+      const { id } = req.params;
+      const columns = ["movie_id", "name", "city", "addres", "show_times", "price", "logo"];
+
+      const request = await cinemasModel.updateCinema(req.body, columns, id);
+
+      if (request.length > 0) {
+        res.status(202).json({
+          status: true,
+          message: "Update cinema success",
+        });
+
+        return;
+      }
+    } catch (error) {
+      res.status(502).json({
+        status: false,
+        message: "Something wrong in our server",
+        data: [],
+      });
+    }
   }
-});
+);
 
 // Delete Cinema (/cinemas/:id)
 router.delete("/cinemas/:id", async (req, res) => {
